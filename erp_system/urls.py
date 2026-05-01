9a724f1e8d5c0673a3ce641f62222502951c4259
2400031832 <2400031832@kluniversity.in>
2026-05-01 09:01:31 +0530

    your message

--- erp_system/urls.py
+++ erp_system/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='login', permanent=False), name='root'),
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('students.urls')),
